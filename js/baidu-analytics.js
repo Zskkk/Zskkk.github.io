d2c5cb36424012f8dd1dce68b18c59ed5c77eebf
Zskkk <409732112@qq.com>
2026-04-19 00:56:07 +0800

    Site updated: 2026-04-19 00:55:51

---
+++ js/baidu-analytics.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?e6b493e52ffc2c61da772aa691472915";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
